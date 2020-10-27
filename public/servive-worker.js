6fd751d3ce16c5d82e3f5cbb21ab12ed065648bd
Joe Lowery <joseph.loweryit@gmail.com>
2020-10-26 21:21:30 -0400

    service worker fix

--- public/servive-worker.js
+++ public/servive-worker.js
@@ -10,12 +10,10 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
     "styles.css"
   ];
-​
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
-​
-// install
-self.addEventListener("install", function (evt) {
+    const CACHE_NAME = "static-cache-v1";
+    const DATA_CACHE_NAME = "data-cache-v1";
+    // install
+    self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches
       .open(CACHE_NAME)
@@ -35,16 +33,15 @@ self.addEventListener("install", function (evt) {
       .catch((err) => {
         console.log(err);
       })
-  );
-  self.skipWaiting();
-});
-// activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+        );
+         self.skipWaiting();
+        });// activate
+        self.addEventListener("activate", function (evt) {
+         evt.waitUntil(
+            caches.keys().then((keyList) => {
+            return Promise.all(
+             keyList.map((key) => {
+             if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
